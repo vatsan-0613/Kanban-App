e4c747a0173cd1ed98cd0283d4fc0c7c0e15e5a5
SRIVATSAN S <95332677+vatsan-0613@users.noreply.github.com>
2023-09-05 03:15:17 +0530

    domain change

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ export default function App(){
   const [isDataFetched, setDataFetched] = React.useState(false);
 
   React.useEffect(() => {
-    fetch("https://kanban-sri.onrender.com/api/getData")
+    fetch("https://kanban-web-app.onrender.com/api/getData")
       .then((response) =>{
         return response.json();
       })
