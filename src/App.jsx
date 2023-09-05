6885cae0b27608fb2b7212bfeaf0cbc3b93ee4d5
SRIVATSAN S <95332677+vatsan-0613@users.noreply.github.com>
2023-09-05 21:47:12 +0530

    spinner aligned at center

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,23 @@
-import Header from "./Header"
-import Card from "./Card"
+import Header from "./Header";
+import Card from "./Card";
 import React from "react";
-import Spinner from 'react-bootstrap/Spinner';
+import Spinner from "react-bootstrap/Spinner";
 
-
-
-export default function App(){
-
+export default function App() {
   const [board, setBoard] = React.useState({
-    todo : [],
-    doing : [],
-    done : []
-  })
-  
+    todo: [],
+    doing: [],
+    done: [],
+  });
+
   const [isDataFetched, setDataFetched] = React.useState(false);
 
   React.useEffect(() => {
     fetch("https://kanban-backend-epgo.onrender.com/api/getData")
-      .then((response) =>{
+      .then((response) => {
         return response.json();
       })
-      .then((data) =>{
+      .then((data) => {
         setBoard(data[0]);
         setDataFetched(true);
       });
@@ -32,23 +29,30 @@ export default function App(){
       [cardName]: updatedTasks,
     }));
   };
-  
-  console.log(board)
 
-  return(
+  console.log(board);
+
+  return (
     <>
       <Header />
-      {isDataFetched ?
-      <div className="cards mt-5">
-      <Card  tasks = {board.todo} cardName={'Todo'} updateCard={updateCard}/>
-      <Card  tasks = {board.doing} cardName={'Doing'} updateCard={updateCard}/>
-      <Card  tasks = {board.done} cardName={'Done'} updateCard={updateCard}/>
-      </div>
-      :  <Spinner animation="grow" variant="info"/>}
+      {isDataFetched ? (
+        <div className="cards mt-5">
+          <Card tasks={board.todo} cardName={"Todo"} updateCard={updateCard} />
+          <Card
+            tasks={board.doing}
+            cardName={"Doing"}
+            updateCard={updateCard}
+          />
+          <Card tasks={board.done} cardName={"Done"} updateCard={updateCard} />
+        </div>
+      ) : (
+        <div
+          class="d-flex justify-content-center align-items-center"
+          style="min-height: 100vh;"
+        >
+          <Spinner animation="grow" variant="info" />
+        </div>
+      )}
     </>
-    
-  )
+  );
 }
-
-
-
